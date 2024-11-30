351d935e22de3ad2fb37af2cf5b2a32ce6835e32
Menaka S <menaka442@gmail.com>
2024-11-30 22:36:09 +0530

    initial commit

---
+++ src/main/java/com/zealtrack/crud_demo/dao/AppDAO.java
@@ -0,0 +1,10 @@
+package com.zealtrack.crud_demo.dao;
+
+import com.zealtrack.crud_demo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+}
